168e6a9752bfe546b23946f5a074296990205dd7
xatoxa <anton.xatuncev@mail.ru>
2023-02-11 19:03:59 +0300

    stages for double screens

--- src/main/java/com/xatoxa/screenshot/Main.java
+++ src/main/java/com/xatoxa/screenshot/Main.java
@@ -2,12 +2,21 @@ package com.xatoxa.screenshot;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application {
     int stateWindow = 0;
@@ -32,6 +41,9 @@ public class Main extends Application {
 
         final SystemTray tray = SystemTray.getSystemTray();
 
+        //для другого экрана
+        Stage anotherStage = new Stage();
+
         //Listener left click XD
         trayIcon.addMouseListener(new MouseAdapter() {
             @Override
@@ -40,9 +52,11 @@ public class Main extends Application {
                     Platform.runLater(() -> {
                         if (stateWindow == 1) {
                             primaryStage.hide();
+                            anotherStage.hide();
                             stateWindow = 0;
                         } else if (stateWindow == 0) {
                             primaryStage.show();
+                            anotherStage.show();
                             stateWindow = 1;
                         }
                     });
@@ -56,6 +70,33 @@ public class Main extends Application {
         } catch (AWTException e) {
             System.out.println("TrayIcon could not be added.");
         }
+
+        ObservableList<Screen> screens = Screen.getScreens();
+
+
+        Rectangle2D bounds = screens.get(0).getVisualBounds();
+
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, bounds.getWidth(), bounds.getHeight());
+
+        scene.setFill(Color.RED);
+
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setOpacity(0.2);
+        primaryStage.setScene(scene);
+
+
+        bounds = screens.get(1).getVisualBounds();
+
+        root = new StackPane();
+        scene = new Scene(root, bounds.getWidth(), bounds.getHeight());
+        scene.setFill(Color.RED);
+        anotherStage.setX(bounds.getMinX());
+        anotherStage.setY(bounds.getMinY());
+
+        anotherStage.initStyle(StageStyle.TRANSPARENT);
+        anotherStage.setOpacity(0.2);
+        anotherStage.setScene(scene);
 
         Platform.setImplicitExit(false);
     }
